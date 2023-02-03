1be05328516392b716e74c7a83babbfc71f19137
d3nd3 <d3nd3second@gmail.com>
2023-02-03 18:42:13 +0000

    sticky

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Navbar from './Navbar.jsx'
 import MainContent from './MainContent.jsx'
 import Footer from './Footer.jsx'
+import './reset.css'
 
 function App() {
   const [count, setCount] = useState(0)
--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -4,58 +4,73 @@ function MainContent() {
 
 	return (
 		<div className="mainContent">
-			<h1>
-				Frank. Honest. Upfront.
-			</h1>
-			<p>
-				&nbsp;Frank Interior Design specialises in the Food &amp; Beverage sector; working on an array of restaurants, bars, caf&eacute;s and coffee shops.
-			</p>
-			<p>
-				&nbsp;Within this sector, Frank naturally understands the importance of a holistic design approach, combining both brand &amp; interior seamlessly.
-			</p>
-
-			<p>
-				&nbsp;All with Frank's philosophy at heart, believing in honest interior design, upfront fees, upfront timescales and straight talking design solutions.
-			</p>
+			<div className="intro">
+				<h1>
+					Frank. Honest. Upfront.
+				</h1>
+				<p>
+					Frank Interior Design specialises in the Food &amp; Beverage sector; working on an array of restaurants, bars, caf&eacute;s and coffee shops.
+				</p>
+				<p>
+					Within this sector, Frank naturally understands the importance of a holistic design approach, combining both brand &amp; interior seamlessly.
+				</p>
+				<p>
+					All with Frank's philosophy at heart, believing in honest interior design, upfront fees, upfront timescales and straight talking design solutions.
+				</p>
+			</div>
 			<h1>
 				What we do.
 			</h1>
-			<h2>
-				- Concept Design
-			</h2>
-			<p>
-				&nbsp;Mood inspiration, interior look &amp; feel, sample board
-			</p>
-			<h2>
-				- Visuals
-			</h2>
-			<p>
-				&nbsp;Bringing a scheme to life with sketch style visuals full of texture
-			</p>
-			<h2>
-				- Proposed CAD Layout
-			</h2>
-			<p>
-				&nbsp;If required site survey of existing, presented back in CAD format with proposed layout planned and designed.
-			</p>
-			<h2>
-				- Planning Application
-			</h2>
-			<p>
-				&nbsp;Full planning, Consent to advertise &amp; Pavement license.
-			</p>
-			<h2>
-				- Tender Drawing Pack
-			</h2>
-			<p>
-				&nbsp;Full set of drawings to issue in order to find a suitable main contractor for the on site stage of the project.
-			</p>
-			<h2>
-				- On Site
-			</h2>
-			<p>
-				&nbsp;In order to ensure a great finished product, on site visits ensure issues are resolved with both practicality and design integrity.
-			</p>
+			<ul>
+				<li>
+					<h2>
+						Concept Design
+					</h2>
+					<p>
+						Mood inspiration, interior look &amp; feel, sample board
+					</p>
+				</li>
+				<li>
+					<h2>
+						Visuals
+					</h2>
+					<p>
+						Bringing a scheme to life with sketch style visuals full of texture
+					</p>
+				</li>
+				<li>
+					<h2>
+						Proposed CAD Layout
+					</h2>
+					<p>
+						If required site survey of existing, presented back in CAD format with proposed layout planned and designed.
+					</p>
+				</li>
+				<li>
+					<h2>
+						Planning Application
+					</h2>
+					<p>
+						Full planning, Consent to advertise &amp; Pavement license.
+					</p>
+				</li>
+				<li>
+					<h2>
+						Tender Drawing Pack
+					</h2>
+					<p>
+						Full set of drawings to issue in order to find a suitable main contractor for the on site stage of the project.
+					</p>
+				</li>
+				<li>
+					<h2>
+						On Site
+					</h2>
+					<p>
+						In order to ensure a great finished product, on site visits ensure issues are resolved with both practicality and design integrity.
+					</p>
+				</li>
+			</ul>
 		</div>
 	)
 }
