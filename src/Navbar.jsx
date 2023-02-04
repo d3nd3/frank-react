835349dc3675f518b743a64aaba21bca41f8a959
d3nd3 <d3nd3second@gmail.com>
2023-02-04 15:59:07 +0000

    only use relative

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,9 +4,9 @@ function Navbar() {
 	return (
 		<div className="navbar">
 			<ul>
-				<li><a href="/">home</a></li>
-				<li><a href="/">work</a></li>
-				<li><a href="/">contact</a></li>
+				<li><a href="./">home</a></li>
+				<li><a href="./">work</a></li>
+				<li><a href="./">contact</a></li>
 			</ul>
 		</div>
 	)
